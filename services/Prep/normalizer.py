e347abc03476d9dfc19ef6a5e03e975208680bf6
cerredz <422michaelcerreto@gmail.com>
2025-07-07 00:02:00 -0400

    updated normalization, also keeping track of original file line numbers

--- services/Prep/normalizer.py
+++ services/Prep/normalizer.py
@@ -1,14 +1,18 @@
 import os
 
-
 # Normalizes files, makes sure that we are only fingerprinting the actual code
 def normalize_file_path(abs_file_path: str):
     code = []
+    line_mapping = []  
+    
     with open(abs_file_path, "r") as file:
         in_multiline_string = False
+        original_line_number = 0
+        
         for line in file:
+            original_line_number += 1
             # Make sure we are only handling code lines 
-            if not is_import(line) and not is_comment(line) and not is_decorator(line) and not is_empty_line(line) and not is_empty_line(line):
+            if not is_import(line) and not is_comment(line) and not is_decorator(line) and not is_empty_line(line):
                 
                 # Check if we are in a multiline string 
                 if is_multiline_string(line):
@@ -16,16 +20,17 @@ def normalize_file_path(abs_file_path: str):
 
                 if not in_multiline_string and not is_multiline_string(line):
                     # Make sure we are not in multiline string or at the closing point of it
-                    code.append(line.lstrip())
+                    normalized_line = line.lstrip()
+                    code.append(normalized_line)
+                    line_mapping.append(original_line_number)
 
-        print(code)
-    return 0
+    return code, line_mapping
 
 def is_import(code: str):
     return code.lstrip().startswith("from") or code.lstrip().startswith("import")
 
 def is_comment(code: str):
-    return code.lstrip().startswith("#")
+    return code.lstrip().startswith("#") or code.lstrip().startswith('"""')
 
 def is_multiline_string(code: str):
     return code.lstrip().startswith("'")
